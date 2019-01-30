29497aa579c1f5bc13bd6941f5e64cdeaf8595d7
yoshida0261 <yoshida261@gmail.com>
2019-01-31 01:06:21 +0900

    feat: app kodein

--- app/src/main/java/jp/co/stah/kodeintest/MainActivity.kt
+++ app/src/main/java/jp/co/stah/kodeintest/MainActivity.kt
@@ -3,13 +3,9 @@ package jp.co.stah.kodeintest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import org.kodein.di.Copy
-import org.kodein.di.Kodein
-import org.kodein.di.KodeinAware
-import org.kodein.di.KodeinContext
+import org.kodein.di.*
 import org.kodein.di.android.closestKodein
 import org.kodein.di.android.retainedKodein
-import org.kodein.di.generic.instance
 import org.kodein.di.generic.kcontext
 
 
@@ -25,12 +21,17 @@ class MainActivity : AppCompatActivity(), KodeinAware {
     }
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val greeting: String by instance()
-        Log.d("tag",greeting)
+    //    val greeting: String by instance()
+    //    Log.d("tag",greeting)
+
+        Log.i("Kodein", "=====================-BINDINGS-=====================")
+        Log.i("Kodein", kodein.container.tree.bindings.description())
+        Log.i("Kodein", "====================================================")
 
     }
 
--- app/src/main/java/jp/co/stah/kodeintest/MyApp.kt
+++ app/src/main/java/jp/co/stah/kodeintest/MyApp.kt
@@ -14,4 +14,20 @@ class MyApp : Application() , KodeinAware {
         bind<String>() with instance("Hello, Kodein!")
     }
 
+    /*
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@DemoApplication))
+
+        bind() from instance(Logger())
+
+        import(electricHeaterModule)
+
+        bind<Coffee>() with provider { Coffee(instance()) }
+
+        // this is bound in the scope of an activity so any retrieval using the same activity will return the same Kettle instance
+        bind<Kettle<Coffee>>() with scoped(WeakContextScope.of<Activity>()).singleton { Kettle<Coffee>(instance(), instance(), instance(), provider()) }
+    }
+
+     */
+
 }
